2b3a6d453ef0a5b51afea4207654330c7f651124
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-06-16 00:23:33 -0400

    ajustes a js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,30 @@
 
-//se especifica tipo de datos a transferir
-function drag(ev) {
-  console.log(ev.target.src);
-  ev.dataTransfer.setData('text', ev.target.id);
-}
+//validación formulario
 
-//(?)
-function permitirDrop(ev){
-  ev.preventDefault();
-}
+document.getElementById("send").addEventListener("click",validate);
 
-//(?)
-function drop(ev) {
-  ev.preventDefault();
-  var id_foto = ev.dataTransfer.getData('text');
-  ev.target.appendChild(document.getElementById(id_foto));
+function validate(){
+	function formularioDom(dato,clase,n){
+		var spanElement = document.createElement("span");
+		var spanText = document.createTextNode("Campo inválido. Ingrese nuevamente su " + dato + ".");
+		var spanFather = document.getElementsByClassName(clase + " input-box")[n];
+		spanElement.appendChild(spanText);
+		spanFather.appendChild(spanElement);
+	}
+
+	function name(){
+		var idNombre = document.getElementById("input-name").value;
+		if (!(/^[A-Z][a-z]{3,19}\s[A-Z][a-z]{3,19}$/).test(idNombre)){
+			formularioDom("nombre","name-container",0);
+		}
+	}
+	name();
+
+	function contrasena(){
+		var idContrasena = document.getElementById("input-password").value;
+		if (idContrasena=="123456" || idContrasena.length<6){
+			formularioDom("contraseña","form-group",0);
+		}
+	}
+	contrasena();
 }
